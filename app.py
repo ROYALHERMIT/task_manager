99c58aa090c9854bca3dd5fa530cf5d7d81ed5ba
Samim09 <samimsam454@gmail.com>
2023-10-29 11:57:24 +0530

    ss

--- app.py
+++ app.py
@@ -1,8 +1,26 @@
-from flask import Flask,render_template
+import mysql.connector
+from flask import Flask, jsonify, render_template, request, redirect, url_for
 import datetime
 
 app = Flask(__name__)
+db_config = {
+    'host': 'localhost',
+    'port': 3307,  
+    'user': 'root',
+    'password': 'root',
+    'database': 'DB_PROBO',
+}
 
+# Create a connection to the database
+
+
+def create_db_connection():
+    return mysql.connector.connect(**db_config)
+
+
+conn = create_db_connection()
+# Create a cursor to interact with the database
+cursor = conn.cursor()
 @app.route('/')
 def hello():
  
