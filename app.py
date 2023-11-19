388732c267ef45b577ef2b3ff9d28646ea7959f8
Rehan07 <rehanalilaskar07@gmail.com>
2023-11-19 21:41:56 +0530

    create project api
    create project api created
    create project ajax request created

--- app.py
+++ app.py
@@ -38,12 +38,10 @@ def get_time_category(current_hour):
     
 @app.route('/signup')
 def signupform():
-
     return render_template('signup.html')
 
 @app.route('/login')
 def loginform():
-
     return render_template('login.html')
 
 @app.route('/project')
@@ -138,6 +136,58 @@ def login_form():
             # User does not exist
             return jsonify({"status": "error", "message": "Invalid credentials."})
 
+    except Exception as e:
+        # Handle any exceptions that may occur during the database operation
+        return jsonify({"status": "error", "message": f"An error occurred: {e}"})
+    finally:
+        # Close the cursor and connection
+        cursor.close()
+        conn.close()
+
+@app.route('/project_form')
+def project_form():
+    project_name = request.form['name']
+    user_id = request.form['id']
+
+    # Create a database connection
+ 
+    conn = mysql.connector.connect(**db_config)
+    cursor = None
+    try:
+        # Use a try-except block to handle any potential errors
+        # Create a cursor to interact with the database
+        cursor = conn.cursor()
+
+        # SQL query to retrieve the user based on the phone number
+        select_query = "SELECT * FROM projects WHERE p_name = %s or user_id = %s"
+
+        # The parameter values
+        p_name = project_name
+        uid = user_id  # Hashed password, adjust accordingly
+        select_values = (p_name, uid)
+        cursor.execute(select_query, select_values)
+        result = cursor.fetchone()
+        if result:
+            return jsonify({"status": "invalid", "message": "User already registered"})
+
+        else:
+            # User does not exist
+            insert_query = "INSERT INTO projects (p_name, user_id) VALUES (%s, %s)"
+            cursor.execute(insert_query, (user_name,user_email,user_phone, user_pass))
+            conn.commit()
+            select_query = "SELECT * FROM projects WHERE p_name = %s"
+
+            # The parameter values
+            select_params = (p_name,)
+
+            # Execute the SELECT query
+            cursor.execute(select_query, select_params)
+            result = cursor.fetchone()
+            print(result)
+            if result:
+                return jsonify({"status": "success", "message": "User registered successfully.","data":result})
+            else:
+                return jsonify({"status": "invalid", "message": "something went wrong"})
     except Exception as e:
         # Handle any exceptions that may occur during the database operation
         return jsonify({"status": "error", "message": f"An error occurred: {e}"})
